a2b25c1458a6e0e9f2260b93b9628fa8ebbeb6bf
iloveooz <iloveooz@yandex.ru>
2019-01-23 22:05:40 +0700

    added clion

---
+++ cmake-build-debug/CMakeFiles/tiho.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tiho.dir/main.cxx.o"
+  "tiho.pdb"
+  "tiho"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tiho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
