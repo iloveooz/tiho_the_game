d6326e9e2f04685728a7ad9aa4e03f490f4ec21f
iloveooz <iloveooz@yandex.ru>
2019-01-23 21:51:26 +0700

    added Clion

--- main.cxx
+++ main.cxx
@@ -7,7 +7,16 @@
 	const int Weight = 1600; // ширина главного окна
 	const int Height = 800;  // высота главного окна
 	
+	const int AmountTanks = 10;
+	
+	struct point {
+		int x;
+		int y;
+	};
+	
 	int main() {
+		
+		srand(time(NULL));
 		
 		std::cout << "start!" << std::endl;
 		
@@ -30,10 +39,25 @@
 		sf::Sprite sCar(tCar);
 		sf::Sprite sTank(tTank);
 		
+		// фигура хексагон
+		sf::CircleShape fHexagon(80, 6);
+		
+		// массив танков
+		point tanks[AmountTanks];
+		
+		// заполняем массив
+		for (int i = 0; i < AmountTanks; i++) {
+			tanks[i].x = rand() % Weight;
+			tanks[i].y = rand() % Height;
+		}		
+		
 		// свойства спрайтов
 		sCar.setPosition(200, 200);
-		sTank.setPosition(200, 300);
-		
+		// sTank.setPosition(200, 300);
+		fHexagon.setPosition(200, 300);
+		fHexagon.setFillColor(sf::Color::Transparent);
+		fHexagon.setOutlineColor(sf::Color::Green);
+		fHexagon.setOutlineThickness(1);
 		while (MWindow.isOpen()) {
 			sf::Event event;
 			
@@ -45,8 +69,13 @@
 			MWindow.clear();
 			
 			MWindow.draw(sBackground);
+			
+			for (int i = 0; i < 8; i++) {
+				sTank.setPosition(tanks[i].x, tanks[i].y);
+				MWindow.draw(sTank);
+			}			
 			MWindow.draw(sCar);
-			MWindow.draw(sTank);
+			MWindow.draw(fHexagon);
 			MWindow.display();
 		}
 		
