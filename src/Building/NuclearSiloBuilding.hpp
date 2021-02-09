33b5a050c3f68f8651adcbc9cb39a8133e9f797d
iloveooz <iloveooz@yandex.ru>
2021-02-09 18:31:49 +0700

    tiho_project: added Nuclear Silo Building

---
+++ src/Building/NuclearSiloBuilding.hpp
@@ -0,0 +1,10 @@
+#ifndef TIHO_NUCLEARSILOBUILDING_HPP
+#define TIHO_NUCLEARSILOBUILDING_HPP
+
+#include "Building.hpp"
+
+class NuclearSiloBuilding : public Building {
+
+};
+
+#endif //TIHO_NUCLEARSILOBUILDING_HPP
