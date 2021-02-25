0023aa319c57d4c5344d4116903521b9f264d8f7
iloveooz <iloveooz@yandex.ru>
2021-02-25 19:35:21 +0700

    tiho_project: added cursor class

---
+++ src/Cursor/Cursor.hpp
@@ -0,0 +1,19 @@
+#ifndef TIHO_CURSOR_HPP
+#define TIHO_CURSOR_HPP
+
+#include "../IObject/IObject.hpp"
+
+namespace ControlGame {
+
+    class Cursor : public IObject {
+    public:
+
+    protected:
+
+    private:
+
+    };
+
+}
+
+#endif //TIHO_CURSOR_HPP
