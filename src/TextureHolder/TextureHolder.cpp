f5745b67834be4f1912bf6c7f380ae9025a7a058
iloveooz <iloveooz@yandex.ru>
2021-03-14 00:13:45 +0700

    tiho_project: changes in texture holder class

--- src/TextureHolder/TextureHolder.cpp
+++ src/TextureHolder/TextureHolder.cpp
@@ -1,1 +1,17 @@
 #include "TextureHolder.hpp"
+
+void Textures::TextureHolder::load(Textures::ID id, const std::string &fileName) {
+    std::unique_ptr<sf::Texture> texture(new sf::Texture());
+    texture->loadFromFile(fileName);
+    m_TextureMap.insert(std::make_pair(id, std::move(texture)));
+}
+
+sf::Texture &Textures::TextureHolder::get(Textures::ID id) {
+    auto found = m_TextureMap.find(id);
+    return *found->second;
+}
+
+const sf::Texture &Textures::TextureHolder::get(Textures::ID id) const {
+    auto found = m_TextureMap.find(id);
+    return *found->second;
+}
--- src/TextureHolder/TextureHolder.hpp
+++ src/TextureHolder/TextureHolder.hpp
@@ -1,8 +1,50 @@
 #ifndef TIHO_TEXTUREHOLDER_HPP
 #define TIHO_TEXTUREHOLDER_HPP
 
-class TextureHolder {
+#include <map>
+#include <memory>
 
-};
+#include <SFML/Graphics.hpp>
+
+#include "../IObject/IObject.hpp"
+
+namespace Textures {
+    enum ID {
+        Empty,
+        Arsenal,
+        Cannon,
+        Factory,
+        Generator,
+        Laboratory,
+        MainBase,
+        Mine,
+        NuclearSilo,
+        OilDerrick,
+        Car,
+        NuclearRocket,
+        SpitFire,
+        Tank,
+        Worker,
+        Water,
+        Meadow,
+        Desert,
+        Mountain,
+        OilSpot,
+        IronSpot,
+        StartPoint,
+        Count
+    };
+
+    class TextureHolder : public IObject {
+    public:
+        void load(Textures::ID id, const std::string& fileName);
+
+        sf::Texture& get(Textures::ID id);
+        const sf::Texture& get(Textures::ID id) const;
+
+    private:
+        std::map<Textures::ID, std::unique_ptr<sf::Texture>> m_TextureMap;
+    };
+}
 
 #endif //TIHO_TEXTUREHOLDER_HPP
